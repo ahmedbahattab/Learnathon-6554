c2486cae66a2e1d69d2b6e0cb1c61568402ac517
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-01-16 00:58:28 +0530

    Create alphanumeric.py

---
+++ Day_04/alphanumeric.py
@@ -0,0 +1,6 @@
+# Write a program to check for alphanumeric characters
+# The isalnum() method returns True if all characters in the string are alphanumeric (either alphabets or numbers). If not, it returns False.
+a="learnathoncohort1"
+b=""
+print(a.isalnum())
+print(b.isalnum())
