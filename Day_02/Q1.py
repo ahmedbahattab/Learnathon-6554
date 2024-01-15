3a73d0a5ea49e3af62ac62be72dd1374c38ad91c
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-01-16 00:28:34 +0530

    Create Q1.py

---
+++ Day_02/Q1.py
@@ -0,0 +1,8 @@
+#Write a program that prints a formatted message using escape characters (This is a Comment)
+# Define variables for the message
+favorite_color = "blue"
+favorite_food = "pizza"
+# Use escape characters to format the message
+formatted_message = "My favorite color is {} and my favorite food is {}.".format(favorite_color, favorite_food)
+# Print the formatted message
+print(formatted_message)
