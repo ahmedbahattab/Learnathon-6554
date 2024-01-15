8102893255d9fa5e930c8ef97b61a2c1a7926a3e
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-01-16 00:36:52 +0530

    Create Q3.py

---
+++ Day_02/Q3.py
@@ -0,0 +1,10 @@
+# Write a Program to calculate area of rectangle
+l= float(input("Enter the length of the rectangle: "))
+b = float(input("Enter the width of the rectangle: "))
+
+# Calculate area
+area = l * b
+
+# Display the results
+print("\nResults:")
+print(f"Area of rectangle is {area}")
