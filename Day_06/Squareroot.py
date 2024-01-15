994b2f8103561fec00db4d4542c655803ef58bd8
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-01-16 01:19:53 +0530

    Create Squareroot.py

---
+++ Day_06/Squareroot.py
@@ -0,0 +1,8 @@
+# Python Program to Find the Square Root
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+
+print(f"Square root of {number} is {square_root}")
