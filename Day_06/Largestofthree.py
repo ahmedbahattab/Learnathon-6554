4bf312cbc3c6865312d0c7c0292300c0a3cb7d3c
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-01-16 01:22:00 +0530

    Create Largestofthree.py

---
+++ Day_06/Largestofthree.py
@@ -0,0 +1,9 @@
+# Python Program to Find the Largest Among Three Numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+largest_number = max(num1, num2, num3)
+
+
+print(f"Largest Number is {largest_number}")
