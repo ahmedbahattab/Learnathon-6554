547a8fa3fd217e6b2a17177bc25cedc9e1012799
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-01-16 01:09:40 +0530

    Create Numbercheck.py

---
+++ Day_05/Numbercheck.py
@@ -0,0 +1,8 @@
+# Write a program that checks if a given number is positive, negative, or zero. If it's positive, print "Positive number." If it's negative, print "Negative number." If it's zero, print "Zero."
+a=int(input("Enter a Number "))
+if(a>0):
+    print('Positive Number')
+elif(a<0):
+    print('Negative Number')
+else:
+    print("Zero")
