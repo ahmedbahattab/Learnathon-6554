2c46004a81d73e376675838a8e4845a1ac521207
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-04-01 22:57:15 +0530

    Create repeateditems.py

---
+++ Day_10/repeateditems.py
@@ -0,0 +1,9 @@
+# Write a Python program to find the repeated items of a tuple
+# Define the tuple
+t = (2, 34, 45, 6, 7, 2, 4, 5, 78, 34, 2)
+# Print the tuple
+print("Original tuple:", t)
+# Count occurrences of the element 2
+count = t.count(2)
+# Print the count
+print("Number of occurrences of 2:", count)
