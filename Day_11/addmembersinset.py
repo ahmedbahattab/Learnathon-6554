2d6f719f9ea0aa3369af71136e878458f7fd9800
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-04-01 23:02:29 +0530

    Create addmembersinset.py

---
+++ Day_11/addmembersinset.py
@@ -0,0 +1,12 @@
+# Write a Python program to add member(s) in a set
+# Define a set
+my_set = {1, 2, 3, 4, 5}
+
+# Adding a single element to the set
+my_set.add(6)
+
+# Adding multiple elements to the set
+my_set.update([7, 8, 9])
+
+# Display the updated set
+print("Updated set:", my_set)
