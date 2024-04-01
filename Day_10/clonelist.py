86ad8eccaadfc10750e0979f272ae54e1e3eaccf
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-04-01 22:15:11 +0530

    Create clonelist.py

---
+++ Day_10/clonelist.py
@@ -0,0 +1,6 @@
+# Write a Python program to clone or copy a list
+Oglist=[1,2,3,4,5,6,7]
+clonelist=Oglist.copy()
+print(Oglist)
+print(clonelist)
+# The copy() method returns a copy of the specified list
