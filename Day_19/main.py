bdc6016e377f0ba9c98e37f76822fc3b93bf8e64
ahmedbahattab <ahmedbahattab10@gmail.com>
2024-04-12 23:56:35 +0530

    Create main.py

---
+++ Day_19/main.py
@@ -0,0 +1,21 @@
+# main.py
+
+import math_operations
+
+# Demonstrating addition
+print("Addition:", math_operations.addition(5, 3))
+
+# Demonstrating subtraction
+print("Subtraction:", math_operations.subtraction(8, 4))
+
+# Demonstrating multiplication
+print("Multiplication:", math_operations.multiplication(6, 2))
+
+# Demonstrating division
+print("Division:", math_operations.division(10, 2))
+
+# Demonstrating division by zero
+print("Division by zero:", math_operations.division(10, 0))
+
+# Accessing the value of PI
+print("Value of PI:", math_operations.PI)
